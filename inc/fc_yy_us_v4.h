614fc6cb32cc4fd83b87d6d27df195eca2727b16
David Mak <david.18.19.21@gmail.com>
2017-05-29 17:54:22 +0800

    Merge IrUltrasoundSenor class from Internal2017/Leslie

---
+++ inc/fc_yy_us_v4.h
@@ -0,0 +1,46 @@
+/* fc_yy_us_v4.h
+ * Copyright (c) 2014-2017 HKUST SmartCar Team
+ * Refer to LICENSE for details
+ *
+ * Author: Leslie Lee (LeeChunHei)
+ *
+ * FcYyUsV4 class
+ * Interface for Freecar YingYang Ultrasonic Sensor v4.
+ *
+ * Usage:
+ * Pass the Pin::Name of the sensor to the constructor. No additional setup
+ * will be required.
+ *
+ */
+
+#ifndef CHASING17_FCYYUSV4_H_
+#define CHASING17_FCYYUSV4_H_
+
+#include "libbase/k60/gpio.h"
+
+using libbase::k60::Gpi;
+using libbase::k60::Pin;
+
+class FcYyUsV4 {
+ public:
+  /**
+   * @param pin Name of Pin connected to the sensor
+   */
+  FcYyUsV4(Pin::Name pin);
+
+  /**
+   * @return The distance measured by the sensor (mm)
+   */
+  const int GetDistance() const { return distance_; }
+
+ private:
+  static void listener(Gpi* gpi);
+
+  Gpi m_pin_;
+  Gpi::Config gpi_config_;
+
+  static uint32_t impulse_start_time_;
+  static int distance_;
+};
+
+#endif  // CHASING17_FCYYUSV4_H_
---
+++ src/fc_yy_us_v4.cpp
@@ -0,0 +1,37 @@
+/* fc_yy_us_v4.cpp
+ * Copyright (c) 2014-2017 HKUST SmartCar Team
+ * Refer to LICENSE for details
+ *
+ * Author: Leslie Lee (LeeChunHei)
+ *
+ * Implementation for FcYyUsV4 class.
+ *
+ */
+
+#include "fc_yy_us_v4.h"
+
+#include "libsc/system.h"
+
+using libsc::System;
+
+uint32_t FcYyUsV4::impulse_start_time_ = 0;
+int FcYyUsV4::distance_ = 0;
+
+void FcYyUsV4::listener(Gpi* gpi) {
+  if (gpi->Get()) {
+    impulse_start_time_ = System::Time();
+  } else {
+    int temp = (System::Time() - impulse_start_time_) * 340;
+    if (temp < 550000 && temp > 20) {
+      distance_ = temp;
+    }
+  }
+}
+
+FcYyUsV4::FcYyUsV4(libbase::k60::Pin::Name pin) {
+  gpi_config_.pin = pin;
+  gpi_config_.interrupt = Pin::Config::Interrupt::kBoth;
+  gpi_config_.isr = listener;
+  gpi_config_.config.set(Pin::Config::kPassiveFilter);
+  m_pin_ = Gpi(gpi_config_);
+}
