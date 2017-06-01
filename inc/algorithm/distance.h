b557055e253a7fe2fce5545a35ac92cd5cd11452
Peter Tse <tsehonam@gmail.com>
2017-06-01 16:44:23 +0800

    merging

---
+++ inc/algorithm/distance.h
@@ -0,0 +1,12 @@
+//
+// Created by peter on 30/5/2017.
+//
+
+#ifndef CHASING17_ALGORITHM_DISTANCE_H_
+#define CHASING17_ALGORITHM_DISTANCE_H_
+
+namespace algorithm {
+	void USIRDemo();
+}
+
+#endif  // CHASING17_ALGORITHM_BT_DISTANCE_H_
