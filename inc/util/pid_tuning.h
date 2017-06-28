821a17b0a6b6b72e62cdffbd69a9fedcfbc995b1
Peter Tse <tsehonam@gmail.com>
2017-06-29 02:23:39 +0800

    Added pid_tuning.cpp

---
+++ inc/util/pid_tuning.h
@@ -0,0 +1,24 @@
+/*
+ * pid_tuning.h
+ *
+ * Author: Peter Tse (mcreng)
+ *
+ * Copyright (c) 2014-2017 HKUST SmartCar Team
+ * Refer to LICENSE for details
+ *
+ * PID tuning with libutil PID classes and MK Wing's Processing tuning code.
+ *
+ */
+
+#ifndef CHASING17_UTIL_PID_TUNING_MAIN_H_
+#define CHASING17_UTIL_PID_TUNING_MAIN_H_
+
+namespace util {
+namespace pid_tuning {
+
+void main();
+
+}  // namespace testground
+}  // namespace util
+
+#endif  // CHASING17_UTIL_PID_TUNING_MAIN_H_
